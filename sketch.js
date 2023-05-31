27e01a750b0d159879528d2614b43fef735a39ef
dejandrea <dejandrea@gmail.com>
2023-05-31 19:43:55 -0300

    animação do barco

--- sketch.js
+++ sketch.js
@@ -11,9 +11,16 @@ var tower, cannon;
 var angle = 50;
 
 var balls = [];
+var boats = [];
+
+//animação do barco navegando
+var boatAnimation = []
+var boatSpriteSheet, boatSpriteData
 
 function preload() {
   bg = loadImage("./assets/background.gif");
+  boatSpriteSheet = loadImage("./assets/boat/boat.png")
+  boatSpriteData = loadJSON("./assets/boat/boat.json")
 }
 
 function setup() {
@@ -28,6 +35,16 @@ function setup() {
   ground = new Ground(0, height - 1, width * 2, 1);
   cannon = new Cannon(180, 110, 110, 70, angle);
   //cannonBall = new CannonBall(cannon.x,cannon.y)
+
+
+  //criando as animações
+  var boatFrames = boatSpriteData.frames
+  for( var i = 0; i < boatFrames.length; i++){
+    var pos = boatFrames[i].position
+    var img = boatSpriteSheet.get(pos.x, pos.y, pos.w, pos.h)
+    boatAnimation.push(img)
+  }
+  
 }
 
 function draw() {
@@ -37,6 +54,7 @@ function draw() {
 
   tower.display();
   ground.display();
+  showBoats();
 
   //cannonBall.display()
 
@@ -74,5 +92,33 @@ function showCannonBalls(ball, index) {
   if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
     Matter.World.remove(world, ball.body);
     balls.splice(index, 1);
+  }
+}
+
+function showBoats() {
+  if (boats.length > 0) {
+    if (
+      boats.length < 4 &&
+      boats[boats.length - 1].body.position.x < width - 300
+    ) {
+      var positions = [-20, -60, -70, -80];
+      var position = random(positions);
+      var boat = new Boat(width, height - 100, 200, 200, position, boatAnimation);
+      boats.push(boat);
+    }
+
+    for (var i = 0; i < boats.length; i++) {
+      Matter.Body.setVelocity(boats[i].body, {
+        x: -0.9,
+        y: 0,
+      });
+
+      
+      boats[i].display();
+      boats[i].animate()
+    }
+  } else {
+    var boat = new Boat(width, height - 100, 200, 200, -20, boatAnimation);
+    boats.push(boat);
   }
 }
